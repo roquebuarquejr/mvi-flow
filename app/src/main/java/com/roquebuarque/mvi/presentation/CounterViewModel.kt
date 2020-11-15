47591abd35c94221e51cd3f1c035d476f6246cd1
roque.buarque <roque.buarque@share-now.com>
2020-11-15 10:33:37 +0100

    mvvm

--- app/src/main/java/com/roquebuarque/mvi/presentation/CounterViewModel.kt
+++ app/src/main/java/com/roquebuarque/mvi/presentation/CounterViewModel.kt
@@ -1,4 +1,34 @@
 package com.roquebuarque.mvi.presentation
 
-class CounterViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.roquebuarque.mvi.data.Counter
+import com.roquebuarque.mvi.data.CounterCallback
+import com.roquebuarque.mvi.data.CounterRepository
+
+class CounterViewModel: ViewModel() {
+
+    private val repository = CounterRepository
+    val error = MutableLiveData<String>()
+    val loading = MutableLiveData<Boolean>()
+    val content = MutableLiveData<Int>()
+
+
+    fun increase(){
+        loading.value = true
+        repository.increase(object : CounterCallback{
+            override fun onSuccess(counter: Counter) {
+                loading.value = false
+                content.value = counter.value
+            }
+
+            override fun onError(throwable: Throwable) {
+                loading.value = false
+                error.value = throwable.message ?: "error"
+            }
+
+        })
+    }
+
+
 }
