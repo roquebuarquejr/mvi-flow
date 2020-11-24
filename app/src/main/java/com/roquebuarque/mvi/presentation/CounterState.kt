b87ef11aa835a030f03316198004232d5125b680
roque.buarque <roque.buarque@share-now.com>
2020-11-24 10:32:32 +0100

    plantuml

--- app/src/main/java/com/roquebuarque/mvi/presentation/CounterState.kt
+++ app/src/main/java/com/roquebuarque/mvi/presentation/CounterState.kt
@@ -2,6 +2,7 @@ package com.roquebuarque.mvi.presentation
 
 import com.roquebuarque.mvi.data.Counter
 
+
 data class CounterState(
     val counter: Counter,
     val syncState: CounterSyncState
@@ -12,4 +13,14 @@ sealed class CounterSyncState {
     object Content : CounterSyncState()
     data class Message(val msg: String) : CounterSyncState()
 }
-
+/*
+@startuml
+   @startuml
+    [*] --> LOADING :INCREASE/DECREASE
+   LOADING --> CONTENT : SUCCESS
+   CONTENT : counter:Counter
+   LOADING --> MESSAGE : ERROR
+   MESSAGE : msg:String
+    @enduml
+@enduml
+ */
